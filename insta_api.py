2e87bb4dffd6d0b3118512a53badc7fbc89919df
wilyuhm <william.keidel@gmail.com>
2017-08-04 13:37:19 -0700

    import and test requests library

--- insta_api.py
+++ insta_api.py
@@ -1,12 +1,20 @@
 #A small web app for searching multiple tags on instagram
 
-from instagram.client import InstagramAPI
-
+import requests
 import settings
 #settings.py includes the following:
 client_id = settings.client_id
 client_secret = settings.client_secret
 access_token = settings.access_token
 
-api = InstagramAPI(access_token=access_token, client_secret=client_secret)
-print api.tag_search(q='sdzoo', count=10)
+base_url = "https://api.instagram.com"
+tag_name = 'sdzoo'
+endpoint_url = "/v1/tags/" + tag_name
+request_url = base_url + endpoint_url + "?access_token=" + access_token
+
+api_return = requests.get(request_url)
+
+if api_return.status_code == 200:
+	print api_return.text
+else :
+	print 'failure, returned: ' + str(api_return.status_code)
