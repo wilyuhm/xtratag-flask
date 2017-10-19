78c628aab0dfdb8bb3b310dd87702578664fa440
william <william.keidel@gmail.com>
2017-10-19 15:24:25 -0700

    updated for python3

--- insta_api.py
+++ insta_api.py
@@ -26,7 +26,7 @@ def get_image_ids(tag_name):
 		image_ids = []
 		# print response_dict['data']
 		for image in response_dict['data']:
-			image_ids.append(image['id'])
+			image_ids.append(image['link'])
 		return image_ids
 		# with open('json_response.json', 'w') as fd:
 		# 	json.dump(api_return.json(), fd, indent=4, sort_keys=True)
@@ -34,11 +34,27 @@ def get_image_ids(tag_name):
 	else:
 		print('failure, returned: ' + str(api_return.status_code))
 
+#GET image oembed code
+#Return oembed code (does not require access_token)
+def get_oembed(image_link):
+	endpoint_url = '/oembed?url=' + image_link
+	request_url = base_url + endpoint_url
+
+	api_return = requests.get(request_url)
+
+	if api_return.status_code == 200:
+		return api_return.text
+	else:
+		print('failure fetching oembed code, returned: ' + str(api_return.status_code))
+
 tags = ['stilts', 'sdzoo', 'jumpingstilts']
-picture_id_dict = defaultdict(list)
+picture_id_dict = defaultdict(list) #create dictionary with key=image_id, value=list of tags
 for tag in tags:
 	id_list = get_image_ids(tag)
 	for id in id_list:
 		picture_id_dict[id].append(tag)
 
-print picture_id_dict
+for image_id, matched_tags in picture_id_dict.items():
+	if len(matched_tags) == len(tags):
+		oembed_code = get_oembed(image_id)
+		print(oembed_code)
