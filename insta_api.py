f58024c29eb4f565d482865e620c49e247b5c529
william <william.keidel@gmail.com>
2017-10-19 14:56:10 -0700

    script acquires image_ids and any tags matching the user query

--- insta_api.py
+++ insta_api.py
@@ -1,6 +1,9 @@
 #A small web app for searching multiple tags on instagram
 
 import requests
+import json
+from collections import defaultdict
+
 import settings
 #settings.py includes the following:
 client_id = settings.client_id
@@ -8,13 +11,34 @@ client_secret = settings.client_secret
 access_token = settings.access_token
 
 base_url = "https://api.instagram.com"
-tag_name = 'sdzoo'
-endpoint_url = "/v1/tags/" + tag_name
-request_url = base_url + endpoint_url + "?access_token=" + access_token
 
-api_return = requests.get(request_url)
+#GET Tags/tag-name/media/recent
+#Return list of image_ids
+def get_image_ids(tag_name):
+	endpoint_url = "/v1/tags/" + tag_name + '/media/recent'
+	request_url = base_url + endpoint_url + "?access_token=" + access_token
 
-if api_return.status_code == 200:
-	print api_return.text
-else :
-	print 'failure, returned: ' + str(api_return.status_code)
+	api_return = requests.get(request_url)
+
+	if api_return.status_code == 200: #healthy response
+		response_dict = api_return.json()
+		num_images = len(response_dict['data'])
+		image_ids = []
+		# print response_dict['data']
+		for image in response_dict['data']:
+			image_ids.append(image['id'])
+		return image_ids
+		# with open('json_response.json', 'w') as fd:
+		# 	json.dump(api_return.json(), fd, indent=4, sort_keys=True)
+		# 	fd.close()
+	else:
+		print('failure, returned: ' + str(api_return.status_code))
+
+tags = ['stilts', 'sdzoo', 'jumpingstilts']
+picture_id_dict = defaultdict(list)
+for tag in tags:
+	id_list = get_image_ids(tag)
+	for id in id_list:
+		picture_id_dict[id].append(tag)
+
+print picture_id_dict
