371a7247278d49365f1fbb03e0738bc92e532296
william <william.keidel@gmail.com>
2017-10-24 17:08:38 -0700

    added micawber example for oembed embedded

--- xtratag_index.py
+++ xtratag_index.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 from micawber.providers import bootstrap_basic
 from micawber.contrib.mcflask import add_oembed_filters
@@ -13,8 +14,15 @@ oembed_providers = bootstrap_basic()
 add_oembed_filters(app, oembed_providers)
 
 @app.route('/')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def example_view():
+    text = request.args.get('text', 'http://www.youtube.com/watch?v=nda_OSWeyn8')
+    html = request.args.get('html', """
+<p>This is a test</p>
+<p>http://www.youtube.com/watch?v=nda_OSWeyn8</p>
+<p>This will get rendered as a link: http://www.youtube.com/watch?v=nda_OSWeyn8</p>
+<p>This will not be modified: <a href="http://www.google.com/">http://www.youtube.com/watch?v=nda_OSWeyn8</a></p>
+    """)
+    return render_template('hello.html', text=text, html=html)
 
 # url_for('static', filename='style.css')
 
